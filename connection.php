00e56ecf1fcbfd02cbcee8f9ef84c6759854acd7
queuing4oranges <katjazenker@gmail.com>
2022-09-17 14:40:22 +0200

    gitignore new

---
+++ connection.php
@@ -0,0 +1,26 @@
+<?php
+/* see in browser:
+1. either phpmyAdmin -> admin and then file or
+2. php -S localhost:8080 (then in browser: localhost:8080/connection.php)
+*/
+
+//stop error reporting and takes the custom ones
+error_reporting(0);
+
+//setting up DB credentials
+$server_name    = "127.0.0.1";
+$mysql_username = "root";
+$mysql_password = "";
+$db_name        = "movies_api";
+
+//connecting to DB
+$connection = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+
+//checking connection + displaying msg
+if (!$connection) {
+    echo '{"message":"Ooops, unable to connect to database"}';
+    //stop when not able to connect:
+    die();
+} else {
+    echo "successfully connected to database";
+}
